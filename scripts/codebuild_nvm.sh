ba5b9177df5ead804b5e8162c6c6f5381c379778
mgarnick1 <markgman008@gmail.com>
2023-07-21 17:38:06 -0600

    just use nvm

--- scripts/codebuild_nvm.sh
+++
@@ -1,9 +0,0 @@
-// You need to create this script file and refer in your build file as I mentioned this in Buildspec.yaml.
-- NVM_DIR="$HOME/.nvm"
-- mkdir -p $NVM_DIR
-- cd $(mktemp -d) 
-- . $NVM_DIR/nvm.sh 
-//Replace the version number with the version you needed. 
-- nvm install 18 
-- nvm use 18 //If you install multiple version of NVM command then you need use this command to use particular version.
-                                
